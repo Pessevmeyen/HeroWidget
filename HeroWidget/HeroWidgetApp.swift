142a9e157ec0ef72a874eebe85f5d3ee2fa0aded
Furkan <gitaristler.toplulugu@gmail.com>
2022-04-29 01:31:44 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Furkan Eruçar on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
